d5c6320c36e9e7683a795c149e21cdb2811d1886
Вячеслав Островский <slslava007@gmail.com>
2021-06-16 23:58:03 +0300

    Added branch logInOstrovskiy

---
+++ UnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WHAT_DP_205_TAQC
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

47bdecefbd0a1dac88154e45f04c7d3eec3efd64
Вячеслав Островский <slslava007@gmail.com>
2021-06-17 14:46:29 +0300

    Added WHAT_PageObject

---
+++ WHAT_PageObject/SigninPage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+
+namespace WHAT_PageObject
+{
+    public class SignInPage : BasePage
+    {
+        /// <summary>
+        /// Locators
+        /// </summary>
+        private By emailLocator = By.Id("email");
+        private By passwordLocator = By.Id("password");
+        private By signinButtonLocator = By.CssSelector("button[type='submit']");
+        private By forgotPasswordLinkLocator = By.CssSelector("a[href='/forgot-password']");
+        private By registrationLinkLocator = By.CssSelector("a[href='/registration']");
+
+        public SignInPage(IWebDriver driver) : base(driver)
+        {
+            
+        }
+
+        public SignInPage fillEmail(string email)
+        {
+            driver.FindElement(emailLocator).SendKeys(email);
+
+            return this;
+        }
+
+        public SignInPage fillPassword(string password)
+        {
+            driver.FindElement(passwordLocator).SendKeys(password);
+
+            return this;
+        }
+
+        //public HomePage enterLoginButton()
+        //{
+        //    driver.FindElement(signinButtonLocator).Click();
+
+        //    return new HomePage(driver);
+        //}
+
+        //public RegistrationPage clickRegistrationLink()
+        //{
+        //    driver.FindElement(registrationLinkLocator).Click();
+
+        //    return new RegistrationPage(driver);
+        //}
+
+        //public ForgotPasswordPage clickForgotPasswordLink()
+        //{
+        //    driver.FindElement(forgotPasswordLinkLocator).Click();
+
+        //    return new ForgotPasswordPage(driver);
+        //}
+    }
+}

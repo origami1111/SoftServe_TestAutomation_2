e677c86503874b22979609dc14b0aaa6b3cb9e84
Paniushkin <dmpn@i.ua>
2021-06-25 18:21:56 +0300

    SecretariesPage

---
+++ WHAT_Tests/SecretariesTests/SecretariesTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using WHAT_PageObject;
+
+
+namespace WHAT_Tests
+{
+    [TestFixture]
+    public class SecretariesTests
+    {
+        
+    }
+}

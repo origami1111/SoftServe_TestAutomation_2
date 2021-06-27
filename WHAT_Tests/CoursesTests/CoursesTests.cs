7d6e8d96d9f1869c35c2134d9ff6deac1e7d22f3
origami1111 <71500001+origami1111@users.noreply.github.com>
2021-06-27 21:12:50 +0300

    Add files via upload

---
+++ WHAT_Tests/CoursesTests/CoursesTests.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using WHAT_PageObject;
+
+namespace WHAT_Tests
+{
+    [TestFixture]
+    public class CoursesTests : TestBase
+    {
+        private CoursesPage coursesPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            coursesPage = new SignIn(driver)
+                            .SignInAsAdmin()
+                            .SidebarNavigateTo<CoursesPage>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            coursesPage.Logout();
+        }
+
+        [Test]
+        public void VerifyCourseDetails()
+        {
+            int courseNumber = 3;
+            string expected =  coursesPage.ReadCourseName(courseNumber);
+            
+            var courseDetailsPage = coursesPage.ClickCourseName(courseNumber);
+            string actual = courseDetailsPage.ReadCourseNameDetails();
+            driver.Navigate().Back();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void EditCourse()
+        {
+            int courseNumber = 3;
+            string courseName = "New course";
+            coursesPage.ClickPencilLink(courseNumber);
+                     //  .FillCourseName(courseName)
+                     //  .ClickCancelButton();
+
+        }
+
+
+        [Test]
+        public void AddCourse()
+        {
+            string courseName = "New course";
+            coursesPage.ClickAddCourseButton()
+                       .FillCourseName(courseName)
+                       .ClickCancelButton();
+
+        }
+    }
+}

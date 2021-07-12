f63dbe4b15783c970240e80b56ce8c08a210884c
Yaricksss <raud256@gmail.com>
2021-07-12 15:17:23 +0300

    added post shedule test

---
+++ WHAT_API/API_Tests/POST_AddShedule_Tests/POST_AddShedule_ValidTest.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using WHAT_Utilities;
+
+namespace WHAT_API
+{
+    [TestFixture]
+    public class POST_AddShedule_ValidTest : API_BaseTest
+    {
+        private CreateSchedule schedule;
+        private RestRequest request;
+        private IRestResponse response;
+
+        [OneTimeSetUp]
+        public void PreConditions()
+        {
+            request = new RestRequest(ReaderUrlsJSON.ByName("ApiSchedules", endpointsPath), Method.POST);
+            request.AddHeader("Authorization", GetToken(Role.Admin));
+
+
+            schedule = new SheduleGenerator()
+                           .GenerateShedule();
+
+            request.AddJsonBody(schedule);
+            response = client.Execute(request);
+        }
+
+        [Test, TestCase(HttpStatusCode.OK)]
+
+        public void POST_ValidData(HttpStatusCode expected)
+        {
+            var actual = response.StatusCode;
+            string stream = response.Content;
+            var jsonSchedule = JsonConvert.DeserializeObject<EventOccurrence>(stream);
+
+            Assert.AreEqual(expected, actual);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(schedule.Pattern.Type, jsonSchedule.Pattern);
+                Assert.AreEqual(schedule.Context.GroupID, jsonSchedule.StudentGroupId);
+                Assert.AreEqual(schedule.Range.StartDate, jsonSchedule.EventStart);
+                Assert.AreEqual(schedule.Range.FinishDate, jsonSchedule.EventFinish);
+
+                foreach (var item in jsonSchedule.Events)
+                {
+                    Assert.AreEqual(item.MentorId, schedule.Context.MentorID);
+                    Assert.AreEqual(item.StudentGroupId, schedule.Context.GroupID);
+                    Assert.AreEqual(item.ThemeId, schedule.Context.ThemeID);
+                    Assert.LessOrEqual(item.EventFinish,schedule.Range.FinishDate);
+                    Assert.GreaterOrEqual(item.EventStart,schedule.Range.StartDate);
+                }
+            });
+        }
+
+    }
+}
---
+++ WHAT_API/API_Tests/POST_AddShedule_Tests/SheduleGenerator.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHAT_API
+{
+    public class SheduleGenerator
+    {
+        public CreateSchedule GenerateShedule()
+        {
+            CreateSchedule shedule = new CreateSchedule
+            {
+                Pattern = new Pattern
+                {
+                    Type = PatternType.Daily,
+                    Interval = 3,
+                    DaysOfWeek = { DayOfWeek.Monday, DayOfWeek.Friday }
+                },
+
+                Range = new OccurrenceRange
+                {
+                    StartDate = new DateTime(2020, 1, 3, 13, 27, 09).ToUniversalTime(),
+                    FinishDate = new DateTime(2021, 7, 7, 15, 27, 09).ToUniversalTime()
+                },
+
+                Context = new Context
+                {
+                    MentorID = 1,
+                    ThemeID = 4,
+                    GroupID = 4
+                }
+            };
+
+            return shedule;
+        }
+    }
+}

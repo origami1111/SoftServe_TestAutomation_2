068779b09346337bdd20ed421ca0685bb5b8e181
Paniushkin <dmpn@i.ua>
2021-07-19 00:33:53 +0300

    GET_AllSchedulesTests POST_AddNewSecretaryTests

---
+++ WHAT_API/API_Tests/SecretariesTests/POST_AddNewSecretary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RestSharp;
+
+namespace WHAT_API.API_Tests.SecretariesTests
+{
+    [TestFixture]
+    class POST_AddNewSecretary : API_BaseTest
+    {
+        [Test]
+        public void VerifyAddSecretary_Valid()
+        {
+
+        }
+
+    }
+}

cf83ed6b26f397d48e48f4e556345d77f4410c2a
Paniushkin <dmpn@i.ua>
2021-07-20 17:19:18 +0300

    Secretary entity AddNewSecretary test

--- WHAT_API/API_Tests/Secretaries/POST_AddNewSecretary.cs
+++ WHAT_API/API_Tests/Secretaries/POST_AddNewSecretary.cs
@@ -24,20 +24,45 @@ namespace WHAT_API.API_Tests
         [TestCase(Role.Secretary)]
         public void VerifyAddingStudentAccount_Valid(Role role)
         {
-            ////POST
-            //var expectedUser = UserGenerator.GenerateUser();
-            //string expectedAvatarUrl = null;
-            //request = new RestRequest(ReaderUrlsJSON.ByName("ApiAccountsReg", endpointsPath), Method.POST);
-            //request.AddJsonBody(expectedUser);
-            //response = client.Execute(request);
-            //log.Info($"POST request to {ReaderUrlsJSON.ByName("ApiAccountsAuth", endpointsPath)}");
+            //POST
+            var expectedUser = UserGenerator.GenerateUser();
+            string expectedAvatarUrl = null;
+            request = new RestRequest(ReaderUrlsJSON.ByName("ApiAccountsReg", endpointsPath), Method.POST);
+            request.AddJsonBody(expectedUser);
+            response = client.Execute(request);
+            log.Info($"POST request to {ReaderUrlsJSON.ByName("ApiAccountsAuth", endpointsPath)}");
 
-            ////GET
-            //request = new RestRequest(ReaderUrlsJSON.ByName("ApiAccountsNotAssigned", endpointsPath), Method.GET);
-            //request.AddHeader("Authorization", GetToken(role));
-            //response = client.Execute(request);
-            //int newUserAccountId = JsonConvert.DeserializeObject<List<RegistrationResponseBody>>(response.Content).Max(s => s.Id); ;
-            //log.Info($"GET request to {ReaderUrlsJSON.ByName("ApiAccountsNotAssigned", endpointsPath)}");
+            //GET
+            request = new RestRequest(ReaderUrlsJSON.ByName("ApiAccountsNotAssigned", endpointsPath), Method.GET);
+            request.AddHeader("Authorization", GetToken(role));
+            response = client.Execute(request);
+            int newUserAccountId = JsonConvert.DeserializeObject<List<RegistrationResponseBody>>(response.Content).Max(s => s.Id); ;
+            log.Info($"GET request to {ReaderUrlsJSON.ByName("ApiAccountsNotAssigned", endpointsPath)}");
+
+            //POST
+            request = new RestRequest(ReaderUrlsJSON.ByName("ApiSecretariesAccountId", endpointsPath), Method.POST);
+            request = InitNewRequest("ApiSecretariesAccountId", Method.POST, GetAuthenticatorFor(role));
+            request.AddUrlSegment("accountId", newUserAccountId.ToString());
+            request.AddParameter("accountId", newUserAccountId);
+            response = client.Execute(request);
+            log.Info($"POST request to {response.ResponseUri}");
+            //GET
+            request = new RestRequest(ReaderUrlsJSON.ByName("ApiSecretariesActive", endpointsPath), Method.GET);
+            request.AddHeader("Authorization", GetToken(role));
+            response = client.Execute(request);
+            log.Info($"GET request to {ReaderUrlsJSON.ByName("ApiSecretariesActive", endpointsPath)}");
+            var activeSecretariesList = JsonConvert.DeserializeObject<List<Secretary>>(response.Content);
+            int maxId = activeSecretariesList.Max(i => i.Id);
+            var actualUser = activeSecretariesList.First(x => x.Id == maxId);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedUser.FirstName, actualUser.FirstName);
+                Assert.AreEqual(expectedUser.LastName, actualUser.LastName);
+                Assert.AreEqual(expectedUser.Email, actualUser.Email);
+                Assert.AreEqual(expectedAvatarUrl, actualUser.AvatarUrl);
+            });
+            log.Info($"Expected and actual results is checked");
+
         }
     }       
 }
---
+++ WHAT_API/Entities/Secretaries/Secretary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WHAT_API.Entities.Secretaries
+{
+    class Secretary
+    {
+    }
+}

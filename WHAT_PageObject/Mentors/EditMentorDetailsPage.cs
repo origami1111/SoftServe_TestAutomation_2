bbee643dcd12a32790fe91f00288858b5150463a
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-04 01:27:03 +0300

    Created Page Object for 'Edit Mentor Details' and 'Mentors' pages

--- WHAT_PageObject/Mentors/EditMentorDetailsPage.cs
+++ WHAT_PageObject/Mentors/EditMentorDetailsPage.cs
@@ -1,12 +1,100 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace WHAT_PageObject
 {
     public class EditMentorDetailsPage : BasePageWithHeaderSidebar
     {
+        #region LOCATORS
+        private By mentorDetailsNavLink = By.LinkText("Mentor details");
+        private By firstNameField = By.XPath("//input[@id='firstName']");
+        private By lastNameField = By.XPath("//input[@id='lastName']");
+        private By emailField = By.XPath("//input[@id='email']");
+        private By groupsInput = By.XPath("//input[@id='groupsInput']");
+        private By addGroupsButton = By.XPath("//button[@id='addGroup']");
+        private By coursesInput = By.XPath("//button[@id='coursesInput']");
+        private By addCourseButton = By.XPath("//button[@id='addCourse']");
+        private By resetButton = By.XPath("//button[@id='resetBtn']");
+        private By saveButton = By.XPath("//button[text()='Save']");
+        private By disableButton = By.XPath("//button[text()='Disable']");
+        #endregion
+
         public EditMentorDetailsPage(IWebDriver driver) : base(driver)
         {
+            
+        }
 
+        private void WaitForMentorToLoad()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            IWebElement firstName = wait.Until(e => e.FindElement(firstNameField));
+        }
+
+        public MentorDetailsPage ClickMentorDetaisNav()
+        {
+            ClickItem(mentorDetailsNavLink);
+            return new MentorDetailsPage(driver);
+        }
+
+        public EditMentorDetailsPage FillFirstNameField(string firstName)
+        {
+            FillField(firstNameField, firstName);
+            return this;
+        }
+
+        public EditMentorDetailsPage FillLastNameField(string lastName)
+        {
+            FillField(lastNameField, lastName);
+            return this;
+        }
+
+        public EditMentorDetailsPage FillEmailField(string email)
+        {
+            FillField(emailField, email);
+            return this;
+        }
+
+        public EditMentorDetailsPage FillGroupsInput(string groupName)
+        {
+            FillField(groupsInput, groupName);
+            return this;
+        }
+
+        public EditMentorDetailsPage FillCoursesInput(string courseName)
+        {
+            FillField(coursesInput, courseName);
+            return this;
+        }
+
+        public EditMentorDetailsPage ClickAddGroupsButton()
+        {
+            ClickItem(addGroupsButton);
+            return this;
+        }
+
+        public EditMentorDetailsPage ClickAddCourseButton()
+        {
+            ClickItem(addCourseButton);
+            return this;
+        }
+
+        public EditMentorDetailsPage ClickResetButton()
+        {
+            ClickItem(resetButton);
+            return this;
+        }
+
+        public EditMentorDetailsPage ClickSaveButton()
+        {
+            ClickItem(saveButton);
+            return this;
+        }
+
+        public EditMentorDetailsPage ClickDisableButton()
+        {
+            ClickItem(disableButton);
+            return this;
         }
     }
 }
--- WHAT_PageObject/Mentors/MentorsPage.cs
+++ WHAT_PageObject/Mentors/MentorsPage.cs
@@ -1,12 +1,167 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace WHAT_PageObject
 {
     public class MentorsPage : BasePageWithHeaderSidebar
     {
+        #region LOCATORS
+        private By disabledMentorsToggle = By.XPath("//input[@type ='checkbox']");
+        private By searchField = By.XPath("//input[@type='text']");
+        private By rowAmountDropdownMenu = By.XPath("//select");
+        private By switchToCardViewButton = By.XPath("//*[contains(@href,'Card.svg')]/parent::*/parent::button");
+        private By switchToTableViewButton = By.XPath("//*[contains(@href,'List.svg')]/parent::*/parent::button");
+        private By addMentorButton = By.XPath("//span[text()='Add a mentor']/parent::button");
+
+        private By previousPageTopButton = By.XPath("//h2[text()='Mentors']/parent::div//button[text()='<']");
+        private By nextPageTopButton = By.XPath("//h2[text()='Mentors']/parent::div//button[text()='>']");
+
+        private By previousPageBottomButton = By.XPath("//div[@class='row mr-0']//button[text()='<']");
+        private By nextPageBottomButton = By.XPath("//div[@class='row mr-0']//button[text()='>']");
+
+        private By sortingRowFirstName = By.XPath("//span[@data-sorting-param='firstName']");
+        private By sortingRowLastName = By.XPath("//span[@data-sorting-param='lastName']");
+        private By sortingRowEmail = By.XPath("//span[@data-sorting-param='email']");
+
+        private By mentorsTable = By.XPath("//table");
+
+        private By mentorsCount = By.XPath("//span[text()[4]=' mentors']");
+        #endregion
+
+        private By mentorFirstName(int rowNumber) => By.XPath($"//tr[{rowNumber}]/td[1]");
+        private By mentorLastName(int rowNumber) => By.XPath($"//tr[{rowNumber}]/td[2]");
+        private By mentorEmail(int rowNumber) => By.XPath($"//tr[{rowNumber}]/td[3]");
+        private By editMentorButton(int rowNumber) => By.XPath($"//tr[{rowNumber}]/td[4]");
+
+        private By pageTopButton(int pageNumber) => By.XPath($"//h2[text()='Mentors']/parent::div//button[text()='{pageNumber}']");
+        private By pageBottomButton(int pageNumber) => By.XPath($"//div[@class='row mr-0']//button[text()='{pageNumber}']");
+
         public MentorsPage(IWebDriver driver) : base(driver)
         {
+            
+        }
 
+        private int GetDisplayedMentorCount()
+        {
+            string mentorCountElement = driver.FindElement(mentorsCount).Text;
+            string[] countComponents = mentorCountElement.Split(" ");
+            int mentorCount;
+            int.TryParse(countComponents[0], out mentorCount);
+            return mentorCount;
+        }
+
+        private int GetTotalMentorCount()
+        {
+            string mentorCountElement = driver.FindElement(mentorsCount).Text;
+            string[] countComponents = mentorCountElement.Split(" ");
+            int mentorCount;
+            int.TryParse(countComponents[2], out mentorCount);
+            return mentorCount;
+        }
+
+        private void WaitMentorsLoad()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            IWebElement firstResult = wait.Until(e => e.FindElement(mentorsTable));
+        }
+
+        public MentorsPage FillSearchField(string searchText)
+        {
+            WaitMentorsLoad();
+            FillField(searchField, searchText);
+            return this;
+        }
+
+        public MentorsPage ClickDisabledMentorsToggle()
+        {
+            //ClickItem(disabledMentorsToggle);
+            //TODO: possibly swap ClickItem code with this:
+            IWebElement element = driver.FindElement(disabledMentorsToggle);
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(element).Click().Build().Perform();
+            return this;
+        }
+
+        public MentorsPage ClickSwitchToCardViewButton()
+        {
+            ClickItem(switchToCardViewButton);
+            return this;
+        }
+
+        public MentorsPage ClickSwitchToTableViewButton()
+        {
+            ClickItem(switchToTableViewButton);
+            return this;
+        }
+
+        public UnassignedUsersPage ClickAddMentorButton()
+        {
+            ClickItem(addMentorButton);
+            return new UnassignedUsersPage(driver);
+        }
+
+        public MentorsPage ClickPreviousPageTopButton()
+        {
+            ClickItem(previousPageTopButton);
+            return this;
+        }
+
+        public MentorsPage ClickNextPageTopButton()
+        {
+            ClickItem(nextPageTopButton);
+            return this;
+        }
+
+        public MentorsPage ClickPreviousPageBottomButton()
+        {
+            ClickItem(previousPageBottomButton);
+            return this;
+        }
+
+        public MentorsPage ClickNextPageBottomButton()
+        {
+            ClickItem(nextPageBottomButton);
+            return this;
+        }
+
+        public MentorsPage ClickSortByFirstName()
+        {
+            ClickItem(sortingRowFirstName);
+            return this;
+        }
+
+        public MentorsPage ClickSortByLastName()
+        {
+            ClickItem(sortingRowLastName);
+            return this;
+        }
+
+        public MentorsPage ClickSortByEmail()
+        {
+            ClickItem(sortingRowEmail);
+            return this;
+        }
+
+        public MentorsPage WaitUntilMentorsTableLoads()
+        {
+            WaitMentorsLoad();
+            return this;
+        }
+
+        public IWebElement verifyMentorsTableExists()
+        {
+            IWebElement table = null;
+            try
+            {
+                table = driver.FindElement(mentorsTable);
+            }
+            catch
+            {
+
+            }
+            return table;
         }
     }
 }

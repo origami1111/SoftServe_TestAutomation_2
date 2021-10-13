4387684e5d820d16060a2884f24ecbbc74aaf821
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-13 13:03:47 +0300

    Change FillField method, Add methods to wait for elements, to click with action, to get page instance

--- WHAT_PageObject/BaseForPages/BasePage.cs
+++ WHAT_PageObject/BaseForPages/BasePage.cs
@@ -1,4 +1,7 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace WHAT_PageObject
 {
@@ -16,6 +19,8 @@ namespace WHAT_PageObject
             var field = driver.FindElement(locator);
             field.Click();
             field.Clear();
+            field.SendKeys(Keys.Control + "a");
+            field.SendKeys(Keys.Delete);
             field.SendKeys(text);
 
         }
@@ -23,6 +28,36 @@ namespace WHAT_PageObject
         protected void ClickItem(By locator)
         {
             driver.FindElement(locator).Click();
+        }
+
+        protected void ActionClickItem(By locator)
+        {
+            IWebElement element = driver.FindElement(locator);
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(element).Click().Perform();
+        }
+
+        public T WaitUntilElementLoads<T>(By locator) where T : BasePage
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            wait.Until(e => e.FindElement(locator));
+            return GetPageInstance<T>(driver);
+        }
+
+        public T ChangePageInstance<T>() where T : BasePage
+        {
+            return GetPageInstance<T>(driver);
+        }
+
+        public T Log<T>(string text) where T : BasePage
+        {
+
+            return GetPageInstance<T>(driver);
+        }
+
+        protected T GetPageInstance<T>(params object[] args) where T : BasePage
+        {
+            return (T)Activator.CreateInstance(typeof(T), args);
         }
     }
 }

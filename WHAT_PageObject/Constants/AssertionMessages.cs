aa91906e33672c1515dc7e764847bf6745a6ddf1
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-17 16:03:21 +0300

    Extract assertion messages into constants

---
+++ WHAT_PageObject/Constants/AssertionMessages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WHAT_PageObject
+{
+    public static class AssertionMessages
+    {
+        public static class EditMentorsDetailsPage
+        {
+            public static string FIRST_NAME_ERROR_MESSAGE = "'First name' field error verification";
+            public static string LAST_NAME_ERROR_MESSAGE = "'Last name' field error verification";
+            public static string EMAIL_ERROR_MESSAGE = "'Email' field error verification";
+            public static string SAVE_BUTTON_ENABLED_MESSAGE = "'Save' button enabled state verification";
+        }
+
+        public static class MentorsPage
+        {
+
+        }
+    }
+}

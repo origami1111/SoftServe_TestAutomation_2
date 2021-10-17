07017e469384203523149618ee6e3d6edfcbc8da
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-18 00:59:24 +0300

    Add VerifyViewingOfMentorDetails test

--- WHAT_PageObject/Mentors/MentorDetailsPage.cs
+++ WHAT_PageObject/Mentors/MentorDetailsPage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
 
@@ -11,6 +12,7 @@ namespace WHAT_PageObject
         private By mentorFirstName = By.XPath(Locators.MentorDetailsPage.FIRST_NAME);
         private By mentorLastName = By.XPath(Locators.MentorDetailsPage.LAST_NAME);
         private By mentorEmail = By.XPath(Locators.MentorDetailsPage.EMAIL);
+        private By mentorDetails = By.XPath(Locators.MentorDetailsPage.MENTOR_DETAILS);
         #endregion
 
         public MentorDetailsPage(IWebDriver driver) : base(driver)
@@ -31,6 +33,48 @@ namespace WHAT_PageObject
                 driver.FindElement(mentorLastName).Text,
                 driver.FindElement(mentorEmail).Text
             };
+        }
+
+        public MentorDetailsPage WaitUntilMentorDetailsLoads()
+        {
+            WaitUntilElementLoads<MentorDetailsPage>(mentorDetails);
+            return this;
+        }
+
+        public MentorDetailsPage VerifyFirstName(string expected)
+        {
+            string actual = GetFirstName();
+            Assert.AreEqual(expected, actual);
+            return this;
+        }
+
+        public MentorDetailsPage VerifyLastName(string expected)
+        {
+            string actual = GetLastName();
+            Assert.AreEqual(expected, actual);
+            return this;
+        }
+
+        public MentorDetailsPage VerifyEmail(string expected)
+        {
+            string actual = GetEmail();
+            Assert.AreEqual(expected, actual);
+            return this;
+        }
+
+        public string GetFirstName()
+        {
+            return driver.FindElement(mentorFirstName).Text;
+        }
+
+        public string GetLastName()
+        {
+            return driver.FindElement(mentorLastName).Text;
+        }
+
+        public string GetEmail()
+        {
+            return driver.FindElement(mentorEmail).Text;
         }
     }
 }
---
+++ WHAT_Tests/MentorsTests/MentorDetailsPage_VerifyViewingOfMentorDetails.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Allure.Core;
+using NUnit.Framework;
+using WHAT_PageObject;
+using WHAT_Utilities;
+
+namespace WHAT_Tests
+{
+    [TestFixture]
+    [AllureNUnit]
+    class MentorDetailsPage_VerifyViewingOfMentorDetails : TestBase
+    {
+        WhatAccount mentor;
+
+        [SetUp]
+        public void Precondition()
+        {
+            var newUser = new GenerateUser();
+            newUser.FirstName = StringGenerator.GenerateStringOfLetters(30);
+            newUser.LastName = StringGenerator.GenerateStringOfLetters(30);
+
+            var unassigned = api.RegistrationUser(newUser);
+            mentor = api.AssignRole(unassigned, Role.Mentor);
+
+            log.Info($"Go to {driver.Url}");
+        }
+
+        [TearDown]
+        public void Postcondition()
+        {
+            api.DisableAccount(mentor, Role.Mentor);
+        }
+
+        [Test, Description("DP213-147")]
+        [TestCase(Role.Admin)]
+        [TestCase(Role.Secretary)]
+        public void TestMentorsTablePage_VerifySortingOfActiveMentors(Role role)
+        {
+            var mentorName = $"{mentor.FirstName} {mentor.LastName}";
+            var credentials = ReaderFileJson.ReadFileJsonCredentials(role);
+            var softAssetions = new SoftAssert();
+            var page = new MentorDetailsPage(driver);
+            new SignInPage(driver)
+                .SignInAsAdmin(credentials.Email, credentials.Password)
+                .SidebarNavigateTo<MentorsPage>()
+                .WaitUntilMentorsTableLoads()
+                .FillSearchField(mentorName)
+                .ClickMentorNameOnRow(1)
+                .WaitUntilMentorDetailsLoads()
+                .SoftAssertAdd<MentorDetailsPage>(
+                        softAssetions,
+                        mentor.FirstName,
+                        page.GetFirstName(),
+                        AssertionMessages.MentorDetailsPage.FIRST_NAME)
+                .SoftAssertAdd<MentorDetailsPage>(
+                        softAssetions,
+                        mentor.LastName,
+                        page.GetLastName(),
+                        AssertionMessages.MentorDetailsPage.LAST_NAME)
+                .SoftAssertAdd<MentorDetailsPage>(
+                        softAssetions,
+                        mentor.Email,
+                        page.GetEmail(),
+                        AssertionMessages.MentorDetailsPage.EMAIL)
+                .SoftAssertAll<MentorDetailsPage>(softAssetions);
+        }
+    }
+}

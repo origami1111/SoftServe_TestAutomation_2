8517259089dbd9b31012b7e4f12c5e3dbd4fd223
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-17 23:56:54 +0300

    Extracted VerifyEditMentor_CorrectData test into separate file

---
+++ WHAT_Tests/MentorsTests/EditMentorDeatilsPage_VerifyEditMentor_CorrectData.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Allure.Core;
+using NUnit.Framework;
+using WHAT_PageObject;
+using WHAT_Utilities;
+
+namespace WHAT_Tests
+{
+    [TestFixture]
+    [AllureNUnit]    
+    class EditMentorDeatilsPage_VerifyEditMentor_CorrectData : TestBase
+    {
+        WhatAccount mentor;
+
+        [SetUp]
+        public void Precondition()
+        {
+            var newUser = new GenerateUser();
+            newUser.FirstName = StringGenerator.GenerateStringOfLetters(30); ;
+            newUser.LastName = StringGenerator.GenerateStringOfLetters(30); ;
+
+            var unassigned = api.RegistrationUser(newUser);
+            mentor = api.AssignRole(unassigned, Role.Mentor);
+
+            log.Info($"Go to {driver.Url}");
+        }
+
+        [TearDown]
+        public void Postcondition()
+        {
+            api.DisableAccount(mentor, Role.Mentor);
+        }
+
+        [Test, Description("DP213-150")]
+        [TestCase(Role.Admin)]
+        [TestCase(Role.Secretary)]
+        public void TestEditMentorDeatilsPage_VerifyEditMentor_CorrectData(Role role)
+        {
+            var credentials = ReaderFileJson.ReadFileJsonCredentials(role);
+            var userName = $"{mentor.FirstName} {mentor.LastName}";
+            var changedFirstName = StringGenerator.GenerateStringOfLetters(30);
+            var changedLastName = StringGenerator.GenerateStringOfLetters(30);
+            var changedEmail = StringGenerator.GenerageEmail();
+
+            new SignInPage(driver)
+                .SignInAsAdmin(credentials.Email, credentials.Password)
+                .SidebarNavigateTo<MentorsPage>()
+                .WaitUntilMentorsTableLoads()
+                .FillSearchField(userName)
+                .ClickEditMentorButtonOnRow(1)
+                .WaitUntilFormLoads()
+                .FillFirstNameField(changedFirstName)
+                .VerifyFirstNameFilled(changedFirstName)
+                .FillLastNameField(changedLastName)
+                .VerifyLastNameFilled(changedLastName)
+                .FillEmailField(changedEmail)
+                .VerifyEmailFilled(changedEmail)
+                .ClickSaveButton();
+        }
+    }
+}

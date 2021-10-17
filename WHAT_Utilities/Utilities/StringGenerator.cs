46650387aa47b2db8939c06a2a43cdbcb5175cfe
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-17 16:01:26 +0300

    Add random string generator class

---
+++ WHAT_Utilities/Utilities/StringGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WHAT_Utilities
+{
+    public static class StringGenerator
+    {
+        public static string GenerateStringOfLetters(int length)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Random random = new Random();
+            int asciiOffset = 97;
+            int asciiRange = 25;
+
+            char letter;
+
+            for (int i = 0; i < length; i++)
+            {
+                int shift = Convert.ToInt32(Math.Floor(asciiRange * random.NextDouble()));
+                letter = Convert.ToChar(shift + asciiOffset);
+                stringBuilder.Append(letter);
+            }
+            return stringBuilder.ToString();
+        }        
+    }
+}

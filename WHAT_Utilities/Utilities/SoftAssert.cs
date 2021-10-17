43734edcd7adc4ca803eee5b1b5e36f66be8b0a9
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-10-17 16:00:55 +0300

    Add soft assertions class

---
+++ WHAT_Utilities/Utilities/SoftAssert.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WHAT_Utilities
+{
+    public class SoftAssert
+    {
+        List<object[]> assertions = new List<object[]>();
+
+        public void Add(Object expected, Object actual, string message = "")
+        {
+            assertions.Add(new object[3]{expected, actual, message});
+        }
+        public void AssertAll()
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (Object[] obs in assertions)
+                {
+                    string errorMessage = obs[2] as string;
+                    Assert.AreEqual(obs[0], obs[1], errorMessage);
+                }
+            });
+        }
+    }
+}

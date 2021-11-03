46db54eede7fef1ef0e662c134a4a39c3c5a1ead
BurmistrovDYu <BurmistrovDYu@gmail.com>
2021-11-03 12:32:44 +0200

    Add Category for TestEditMentorDeatilsPage_VerifyEditMentor_CorrectData

--- WHAT_Tests/MentorsTests/EditMentorDeatilsPage_VerifyEditMentor_CorrectData.cs
+++ WHAT_Tests/MentorsTests/EditMentorDeatilsPage_VerifyEditMentor_CorrectData.cs
@@ -6,6 +6,7 @@ using WHAT_Utilities;
 namespace WHAT_Tests
 {
     [TestFixture]
+    [Category("FrontEndTest-Mentors")]
     [AllureNUnit]    
     class EditMentorDeatilsPage_VerifyEditMentor_CorrectData : TestBase
     {
